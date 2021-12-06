7f3a8a931679c1a9a09eb3d98bb68fa61aa0b1f6
JacobKnaack <jacobknck@gmail.com>
2021-12-06 14:04:24 -0800

    updates index with initial project pathing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const app = require('./src/server');
-const { db } = require('./src/models');
+const app = require('./lib/server');
+const { db } = require('./lib/model');
 const PORT = process.env.PORT || 3003;
 
 db.sync().then(() => {
