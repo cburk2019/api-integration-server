0c54841d4689f0c6905d46fb828a4dede27ff843
Christopher Burk <c.burk.tech@gmail.com>
2021-12-06 18:14:15 -0800

    removes commented out code

--- __tests__/authrouter.test.js
+++ __tests__/authrouter.test.js
@@ -13,12 +13,10 @@ const bearerAuth = require('../lib/middleware/bearer.js');
 
 beforeAll(async () => {
   await db.sync();
-  // done();
 });
 
 afterAll(async () => {
   await db.drop();
-  // done();
 });
 
 let users = {
@@ -30,7 +28,6 @@ describe('Auth Router', () => {
 
   it('can add an item to the DB if user has create permissions', async () => {
     const user = await Users.create(users['admin']);
-    // test bearerAuth
     let req = {
       headers: {
         authorization: `Bearer: ${user.token}`
@@ -48,38 +45,5 @@ describe('Auth Router', () => {
     await bearerAuth(req, res, next);
 
     expect(next).toHaveBeenCalled();
-
-    // const token = jwt.sign(users['admin'], process.env.SECRET);
-    // await server.post('/signup').send(users['writer']);
-
-    // const body = {};
-
-    // const res = await server.post('/api/test')
-    //   .set('Authorization', 'Bearer ' + token).send(body)
-
-    // expect(res.statusCode).toBe(201);
-    // expect(res.body).toBe(body);
-
-    // const token2 = jwt.sign(users['user'], process.env.SECRET);
-    // await server.post('/signup').send(users['user']);
-
-    // const res2 = await server.post('/api/test')
-    //   .set('Authorization', 'Bearer ' + token2).send(body)
-
-    // expect(res2.statusCode).toBe(500);
-
   });
-
-  // it('can get if user has read permissions =', async (done) => {
-  //   const body = {};
-
-  //   const token = jwt.sign(users['user'], process.env.SECRET);
-
-  //   const res = await server.get('/signup').set('Authorization', 'Bearer ' + token);
-
-  //   expect(res.statusCode).toBe(200);
-  //   expect(res.body).toBe(body);
-
-  // });
-
 });
